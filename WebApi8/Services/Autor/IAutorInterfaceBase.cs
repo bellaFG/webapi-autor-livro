fd86806d326ef8edb9e74f1f68c6f7cd1a4c5b08
bellaFG <isabella.ferzales@gmail.com>
2025-03-31 22:04:35 -0300

    API de Autores e Livros

---
+++ WebApi8/Services/Autor/IAutorInterfaceBase.cs
@@ -0,0 +1,10 @@
+﻿using WebApi8.Models;
+
+namespace WebApi8.Services.Autor
+{
+    public class IAutorInterfaceBase
+    {
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor);
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idLivro);
+    }
+}
